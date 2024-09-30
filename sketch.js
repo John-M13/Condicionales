0047056731a5586df9edb24437ca29b724a428e6
John Macas <john.macas3@ucuenca.edu.ec>
2024-09-30 16:47:15 -0500

    rebote de pelota

--- sketch.js
+++ sketch.js
@@ -1,4 +1,27 @@
+let x = 200;
+let y = 200;
+let xspeed = 20;
+let yspeed = 20;
+
 function setup() {
-  createCanvas(windowWidth, windowHeigth);
+  createCanvas(windowWidth, windowHeight);
 }
-function draw() {}
+function draw() {
+    background(210, 255, 114);
+    fill(84, 195, 146);
+    noStroke()
+    ellipse(x, y, 100, 100);
+    // Movimiento
+  x += xspeed;
+  y += yspeed;
+  
+  // Rebotar en los bordes
+  
+  if (x > width - 55 || x < 5) {
+    xspeed *= -1;
+  }
+  if (y > height - 5|| y < 5) {
+    yspeed *= -1;
+
+}
+}
